21faf6b41c1aa7e8e427aab634b7e9a0b616f883
Robert McHardy <admin@noobzunited.de>
2023-04-29 18:12:59 +0100

    feature: implement function to get best-rq targets

--- best_rq/best_rq.py
+++ best_rq/best_rq.py
@@ -21,7 +21,7 @@ class BestRQMasking:
         """
         self.projection = torch.empty(
             emb_dim, codebook_dim, requires_grad=False, device=device
-        )
+        )  # Shape: (emb_dim, codebook_dim)
         torch.nn.init.xavier_normal_(self.projection)
         self.codebooks = torch.normal(
             mean=0,
@@ -29,7 +29,25 @@ class BestRQMasking:
             size=(num_codebooks, num_targets, codebook_dim),
             requires_grad=False,
             device=device,
-        )
+        )  # Shape: (num_codebooks, num_targets, codebook_dim)
         self.codebooks /= torch.linalg.vector_norm(
             self.codebooks, ord=2, dim=-1, keepdim=True
-        )
+        )  # Shape: (num_codebooks, num_targets, codebook_dim)
+
+    def get_targets(self, in_feats: torch.Tensor) -> torch.Tensor:
+        """Computes the Best-RQ targets for a given tensor of unmasked speech input features.
+
+        :param in_feats: A tensor holding the unmasked speech input features. Shape: (batch_size, seq_length, emb_dim)
+        :return: A tensor holding the computed targets. Shape: (batch_size, num_codebooks, seq_length)
+        """
+
+        proj_feats = (
+            in_feats @ self.projection
+        )  # Shape: (batch_size, seq_length, codebook_dim)
+        proj_feats /= torch.linalg.vector_norm(
+            proj_feats, ord=2, dim=-1, keepdim=True
+        )  # Shape: (batch_size, seq_length, codebook_dim)
+        targets = torch.argmin(
+            self.codebooks[None, :] - proj_feats[:, None, :], dim=-1
+        )  # Shape: (batch_size, num_codebooks, seq_length)
+        return targets
