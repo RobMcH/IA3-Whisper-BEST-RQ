5fa88f0793a2e290571a1d3181446022b48a2848
Robert McHardy <admin@noobzunited.de>
2023-05-07 13:39:13 +0100

    refactor: add additional optimization hyper-parameters to CLI

--- best_rq/train.py
+++ best_rq/train.py
@@ -12,6 +12,7 @@ def parse_args() -> argparse.Namespace:
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
+    # Model hyper-parameters.
     parser.add_argument(
         "--model_name",
         type=str,
@@ -19,12 +20,7 @@ def parse_args() -> argparse.Namespace:
         choices=_MODELS.keys(),
         help="The pre-trained Whisper checkpoint to load.",
     )
-    parser.add_argument(
-        "--batch_size",
-        type=int,
-        default=8,
-        help="The batch size used for BEST-RQ training.",
-    )
+    # BEST-RQ hyper-parameters.
     parser.add_argument(
         "--num_targets",
         type=int,
@@ -48,6 +44,31 @@ def parse_args() -> argparse.Namespace:
         type=int,
         default=2,
         help="The fold of temporal downsampling performed by the encoder.",
+    )
+    # Optimizer/training hyper-parameters.
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=8,
+        help="The batch size used for BEST-RQ training.",
+    )
+    parser.add_argument(
+        "--learning_rate",
+        type=float,
+        default=0.004,
+        help="The peak learning rate to use by the Transformer learning rate scheduler.",
+    )
+    parser.add_argument(
+        "--accumulate_gradients",
+        type=int,
+        default=1,
+        help="The number of batches to accumulate before updating the parameters.",
+    )
+    parser.add_argument(
+        "--warmup_steps",
+        type=int,
+        default=25000,
+        help="The number of warmup steps to use by the Transformer learning rate scheduler.",
     )
     parser.add_argument(
         "--seed",
